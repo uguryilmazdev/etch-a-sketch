31474a7a26023e37c413d07dd4075d49ad2502c2
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-08-15 15:31:28 +0300

    Complete sketch box paint events

--- indexJavaScript.js
+++ indexJavaScript.js
@@ -5,38 +5,64 @@ let sketchSideLength = document.querySelector("#sketch-container").offsetWidth;
 
 slider.addEventListener("input", () => {
 
+    resetSketchContainer(sketchContainer);
+
+    document.querySelector("#sliderText").innerText = `${slider.value} x ${slider.value}`;
+
+    addBoxToSketchContainer(slider, sketchContainer);
+
+    // catch if mousedown-mouseup on boxes
+    let isClicked = false;
+
+    window.addEventListener("mousedown", () => {
+        isClicked = true;
+    })
+
+    window.addEventListener("mouseup", () => {
+        isClicked = false;
+    })
+
+    // set mouse events to paint boxes
+    const boxes = document.querySelectorAll(".sketchBox");
+
+    for (let i = 0; i < boxes.length; i++) {
+
+        boxes[i].addEventListener("click", () => {
+            boxes[i].style.backgroundColor = "red";
+        })
+
+        boxes[i].addEventListener('mousemove', () => {
+            if (isClicked) {
+                boxes[i].style.backgroundColor = "red";
+            }
+        })
+    }
+})
+
+// functions
+function resetSketchContainer(sketchContainer) {
+
     let child = sketchContainer.lastElementChild;
 
     while (child) {
         sketchContainer.removeChild(child);
         child = sketchContainer.lastElementChild;
     }
+}
 
-    document.querySelector("#sliderText").innerText = `${slider.value} x ${slider.value}`;
+function addBoxToSketchContainer(slider, sketchContainer) {
 
     let pixelRatio = sketchSideLength / slider.value;
-    console.log(pixelRatio)
-    
+
     for (let i = 0; i < (sketchSideLength / pixelRatio) ** 2; i++) {
-        //NOT elementin const olması lazım!
-        const divTag = document.createElement("div"); 
-        divTag.classList.add("sketchBox");      
-        divTag.innerText = `${i}`;
+
+        const divTag = document.createElement("div");
+        divTag.classList.add("sketchBox");
         divTag.style.width = `${pixelRatio}` + "px";
         divTag.style.height = `${pixelRatio}` + "px";
         /*divTag.style.border = "thick solid blue";*/
         sketchContainer.appendChild(divTag);
+
     }
 
-    const tags = document.querySelectorAll(".sketchBox");
-
-    for (let i = 0; i < tags.length; i++) {
-        
-        tags[i].addEventListener('mouseover', () => {
-            tags[i].style.backgroundColor = "red";
-        })
-    }
-
-    // slide yapılınca sıfırlamıyor, altına ekliyor
-})
-
+}
