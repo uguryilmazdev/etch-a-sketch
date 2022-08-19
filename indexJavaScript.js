61d1e3071e6cf9f0d87c8be5af4e772e6def4806
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-08-19 14:54:39 +0300

    Connect pen color pallette to sketch area

--- indexJavaScript.js
+++ indexJavaScript.js
@@ -1,7 +1,24 @@
 const slider = document.querySelector("#slider");
 const sketchContainer = document.querySelector("#sketch-container");
 
+// buttons
+const penColor = document.querySelector("#penColor");
+const backColor = document.querySelector("#backgroundColor");
+const colorModeBtn = document.querySelector("#colorModeBtn");
+const eraserBtn = document.querySelector("#eraserBtn");
+const clearBtn = document.querySelector("#clearBtn");
+const toggleGridBtn = document.querySelector("#toggleGridBtn");
+
+// -----------------------------------------------------------------
+// globals
+let colorPen = "";
+
 let sketchSideLength = document.querySelector("#sketch-container").offsetWidth;
+
+//----------------------------------------------------------------
+penColor.addEventListener("input", () => {
+    colorPen = penColor.value;
+})
 
 slider.addEventListener("input", () => {
 
@@ -27,15 +44,16 @@ slider.addEventListener("input", () => {
 
     for (let i = 0; i < boxes.length; i++) {
 
-        boxes[i].addEventListener("click", () => {
-            boxes[i].style.backgroundColor = "red";
-        })
+            boxes[i].addEventListener("click", () => {
+                boxes[i].style.backgroundColor = colorPen;
+            });
+    
+            boxes[i].addEventListener('mousemove', () => {
+                if (isClicked) {
+                    boxes[i].style.backgroundColor = colorPen;
+                }
+            })
 
-        boxes[i].addEventListener('mousemove', () => {
-            if (isClicked) {
-                boxes[i].style.backgroundColor = "red";
-            }
-        })
     }
 })
 
@@ -66,3 +84,5 @@ function addBoxToSketchContainer(slider, sketchContainer) {
     }
 
 }
+
+
